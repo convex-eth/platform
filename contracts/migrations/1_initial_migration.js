c47d01648599b308764724b83df7ba423d9b8295
c2tp <C2tP@protonmail.com>
2021-05-01 09:08:40 +0900

    add airdrop data

--- contracts/migrations/1_initial_migration.js
+++ contracts/migrations/1_initial_migration.js
@@ -37,7 +37,7 @@ module.exports = function (deployer, network, accounts) {
 	let convexMultisig = "0xa3C5A1e09150B75ff251c1a7815A07182c3de2FB";
 	let convexVoterProxy = "0x989AEb4d175e16225E39E87d0D97A3360524AD80";
 
-	let merkleRoot = "0xb28bc11190214e137fc59e1857e8ffb749934c8363229795895226926386e2b2";
+	let merkleRoot = "0x5b871061b3098656b904a645f5f82c28e31901d78b42685c6fd3d60104d5ed08";
 
 	let crv = "0xD533a949740bb3306d119CC777fa900bA034cd52";
 	let vecrvFeeDistro = "0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc";
