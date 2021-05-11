eb2c66938b032899dd1c8542f40fefb879a73c7d
c2tp <C2tP@protonmail.com>
2021-05-11 18:45:47 +0900

    simple proxy contract for control over treasury funds

---
+++ contracts/contracts/TreasuryFunds.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+
+//receive treasury funds. operator can withdraw
+//allow execute so that certain funds could be staked etc
+contract TreasuryFunds{
+    using SafeERC20 for IERC20;
+    using Address for address;
+
+    address public operator;
+    event WithdrawTo(address indexed user, uint256 amount);
+
+    constructor() public {
+        operator = msg.sender;
+    }
+
+    function setOperator(address _op) external {
+        require(msg.sender == operator, "!auth");
+        operator = _op;
+    }
+    
+    function withdrawTo(IERC20 _asset, uint256 _amount, address _to) external {
+    	require(msg.sender == operator, "!auth");
+
+        _asset.safeTransfer(_to, _amount);
+        emit WithdrawTo(_to, _amount);
+    }
+
+    function execute(
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) external returns (bool, bytes memory) {
+        require(msg.sender == operator,"!auth");
+
+        (bool success, bytes memory result) = _to.call{value:_value}(_data);
+
+        return (success, result);
+    }
+
+}
