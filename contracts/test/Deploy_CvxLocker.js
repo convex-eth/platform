9811ff6cc1ac7427ec224dd8fa83dfb7492623cc
c2tp <C2tP@protonmail.com>
2021-09-02 13:50:03 +0900

    print call data after deployment for fee changes

--- contracts/test/Deploy_CvxLocker.js
+++ contracts/test/Deploy_CvxLocker.js
@@ -3,6 +3,7 @@ const { BN, time } = require('openzeppelin-test-helpers');
 var jsonfile = require('jsonfile');
 var contractList = jsonfile.readFileSync('./contracts.json');
 
+const Booster = artifacts.require("Booster");
 const CvxLocker = artifacts.require("CvxLocker");
 const CvxStakingProxy = artifacts.require("CvxStakingProxy");
 const cvxRewardPool = artifacts.require("cvxRewardPool");
@@ -18,6 +19,7 @@ contract("Deploy CVX Locker", async accounts => {
     let addressZero = "0x0000000000000000000000000000000000000000"
 
     //system
+    let booster = await Booster.at(contractList.system.booster);
     let cvx = await IERC20.at(contractList.system.cvx);
     let cvxcrv = await IERC20.at(contractList.system.cvxCrv);
     let cvxrewards = await cvxRewardPool.at(contractList.system.cvxRewards);
@@ -37,6 +39,34 @@ contract("Deploy CVX Locker", async accounts => {
     await locker.addReward(cvxcrv.address, stakeproxy.address, true, {from:deployer});
     await locker.setStakingContract(stakeproxy.address,{from:deployer});
     await locker.setApprovals();
+    await locker.transferOwnership(multisig,{from:deployer});
+    await locker.owner().then(a=>console.log("owner: " +a));
+
+
+    console.log("redirect extra incentives..");
+    let setTreasuryData = booster.contract.methods.setTreasury(stakeproxy.address).encodeABI();
+    console.log("setTreasuryData: " +setTreasuryData);
+    
+    let setFeesData = booster.contract.methods.setFees(1000, 500, 100, 100).encodeABI();
+    console.log("setFeesData: " +setFeesData);
+
+
+    //quick test that fees are going to their new home
+    // await booster.setTreasury(stakeproxy.address,{from:multisig,gasPrice:0});
+    // await booster.setFees(1000, 500, 100, 100,{from:multisig,gasPrice:0});
+
+    // console.log("test harvest");
+    // await booster.earmarkRewards(38);
+
+    // await crv.balanceOf(stakeproxy.address).then(a=>console.log("extra incentive on stakeproxy: " +a))
+    // await cvxcrv.balanceOf(locker.address).then(a=>console.log("cvxcrv on locker: " +a))
+
+    // await stakeproxy.distribute();
+    // await crv.balanceOf(stakeproxy.address).then(a=>console.log("crv on stakeproxy: " +a))
+    // await crv.balanceOf(locker.address).then(a=>console.log("crv incentive on locker: " +a))
+    // await cvxcrv.balanceOf(locker.address).then(a=>console.log("cvxcrv on locker: " +a))
+
+
     console.log("setup complete");
 
   });
