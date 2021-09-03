34f91f15b016d78510138621b25f4fb72f67b849
c2tp <C2tP@protonmail.com>
2021-09-03 23:58:22 +0900

    add interfaces

--- contracts/contracts/interfaces/IBentoBox.sol
+++ contracts/contracts/interfaces/IBentoBox.sol
@@ -3,4 +3,11 @@ pragma solidity 0.6.12;
 
 interface IBentoBox {
     function toAmount(address _token, uint256 _share, bool _roundUp) external view returns (uint);
+    function deposit(
+        address token_,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external returns (uint256 amountOut, uint256 shareOut);
 }
--- contracts/contracts/interfaces/ICauldron.sol
+++ contracts/contracts/interfaces/ICauldron.sol
@@ -3,4 +3,9 @@ pragma solidity 0.6.12;
 
 interface ICauldron {
     function userCollateralShare(address account) external view returns (uint);
+    function addCollateral(
+        address to,
+        bool skim,
+        uint256 share
+    ) external;
 }
