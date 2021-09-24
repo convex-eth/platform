2ad908ddca122397a49bff90e7e133c8d25b406a
c2tp <C2tP@protonmail.com>
2021-09-24 17:10:09 +0900

    add to locker interface

--- contracts/contracts/interfaces/ILockedCvx.sol
+++ contracts/contracts/interfaces/ILockedCvx.sol
@@ -5,4 +5,7 @@ interface ILockedCvx{
     function lock(address _account, uint256 _amount, uint256 _spendRatio) external;
     function processExpiredLocks(bool _relock, uint256 _spendRatio, address _withdrawTo) external;
     function getReward(address _account, bool _stake) external;
+    function balanceAtEpochOf(uint256 _epoch, address _user) view external returns(uint256 amount);
+    function totalSupplyAtEpoch(uint256 _epoch) view external returns(uint256 supply);
+    function epochCount() external view returns(uint256);
 }
