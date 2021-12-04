b377106d57df241886d6f2c58e69892d6c204bad
c2tp <C2tP@protonmail.com>
2021-12-04 23:44:47 +0900

    lock admin contract to add require checks and to seal off changes to staking proxy

---
+++ contracts/contracts/LockerAdmin.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./interfaces/ILockedCvx.sol";
+
+/*
+Admin proxy for locker contract to fix require checks and seal off staking proxy changes
+*/
+contract LockerAdmin{
+
+    ILockedCvx public constant locker = ILockedCvx(0xD18140b4B819b895A3dba5442F959fA44994AF50);
+    address public operator;
+
+    constructor() public {
+        operator = address(0xa3C5A1e09150B75ff251c1a7815A07182c3de2FB);
+    }
+
+    modifier onlyOwner() {
+        require(operator == msg.sender, "!auth");
+        _;
+    }
+
+    function setOperator(address _operator) external onlyOwner{
+        operator = _operator;
+    }
+
+
+    function addReward(
+        address _rewardsToken,
+        address _distributor,
+        bool _useBoost
+    ) external onlyOwner{
+        locker.addReward(_rewardsToken, _distributor, _useBoost);
+    }
+
+    function approveRewardDistributor(
+        address _rewardsToken,
+        address _distributor,
+        bool _approved
+    ) external onlyOwner{
+        locker.approveRewardDistributor(_rewardsToken, _distributor, _approved);
+    }
+
+    //seal setStakingContract off, make it immutable
+    // function setStakingContract(address _staking) external onlyOwner{
+    //     locker.setStakingContract(_staking);
+    // }
+
+    function setStakeLimits(uint256 _minimum, uint256 _maximum) external onlyOwner {
+        require(_minimum <= _maximum, "min range");
+        locker.setStakeLimits(_minimum, _maximum);
+    }
+
+    function setBoost(uint256 _max, uint256 _rate, address _receivingAddress) external onlyOwner {
+        require(_max < 1500, "over max payment"); //max 15%
+        require(_rate < 30000, "over max rate"); //max 3x
+        locker.setBoost(_max, _rate, _receivingAddress);
+    }
+
+    function setKickIncentive(uint256 _rate, uint256 _delay) external onlyOwner {
+        locker.setKickIncentive(_rate, _delay);
+    }
+
+    function shutdown() external onlyOwner {
+        locker.shutdown();
+    }
+
+    function recoverERC20(address _tokenAddress, uint256 _tokenAmount) external onlyOwner {
+        locker.recoverERC20(_tokenAddress, _tokenAmount);
+    }
+}
--- contracts/contracts/interfaces/ILockedCvx.sol
+++ contracts/contracts/interfaces/ILockedCvx.sol
@@ -11,4 +11,20 @@ interface ILockedCvx{
     function checkpointEpoch() external;
     function balanceOf(address _account) external view returns(uint256);
     function totalSupply() view external returns(uint256 supply);
+
+    function addReward(
+        address _rewardsToken,
+        address _distributor,
+        bool _useBoost
+    ) external;
+    function approveRewardDistributor(
+        address _rewardsToken,
+        address _distributor,
+        bool _approved
+    ) external;
+    function setStakeLimits(uint256 _minimum, uint256 _maximum) external;
+    function setBoost(uint256 _max, uint256 _rate, address _receivingAddress) external;
+    function setKickIncentive(uint256 _rate, uint256 _delay) external;
+    function shutdown() external;
+    function recoverERC20(address _tokenAddress, uint256 _tokenAmount) external;
 }
