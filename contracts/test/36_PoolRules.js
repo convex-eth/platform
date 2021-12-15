02452a78a570ffbc5047de26954a8b002811fbc6
c2tp <C2tP@protonmail.com>
2021-12-15 11:30:03 +0900

    fix test description

--- contracts/test/36_PoolRules.js
+++ contracts/test/36_PoolRules.js
@@ -16,24 +16,13 @@ const ICurveGauge = artifacts.require("ICurveGauge");
 const PoolManagerV3 = artifacts.require("PoolManagerV3");
 
 
-contract("setup lock contract", async accounts => {
-  it("should setup lock contract", async () => {
+contract("deploy pool manager layer", async accounts => {
+  it("should check that pool addresses are properly enforced", async () => {
 
     let deployer = "0x947B7742C403f20e5FaCcDAc5E092C943E7D0277";
     let multisig = "0xa3C5A1e09150B75ff251c1a7815A07182c3de2FB";
     let treasury = "0x1389388d01708118b497f59521f6943Be2541bb7";
     let addressZero = "0x0000000000000000000000000000000000000000"
-
-    //system
-    // let cvx = await IERC20.at(contractList.system.cvx);
-    // let cvxcrv = await IERC20.at(contractList.system.cvxCrv);
-    // let cvxrewards = await cvxRewardPool.at(contractList.system.cvxRewards);
-    // let cvxcrvrewards = await cvxRewardPool.at(contractList.system.cvxCrvRewards);
-    // let crv = await IERC20.at("0xD533a949740bb3306d119CC777fa900bA034cd52");
-    // let exchange = await IExchange.at("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F");
-    // let exchangerouter = await IUniswapV2Router01.at("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F");
-    // let weth = await IERC20.at("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
-    // let dai = await IERC20.at("0x6B175474E89094C44Da98b954EedeAC495271d0F");
 
     let userA = accounts[0];
     let userB = accounts[1];
