599086d20f99ba8327c191ea9b81660f4bd4bc3e
c2tp <C2tP@protonmail.com>
2022-05-08 21:23:24 +0900

    fxs reward hook get current booster instead of hardcode

--- contracts/contracts/FxsRewardHook.sol
+++ contracts/contracts/FxsRewardHook.sol
@@ -7,11 +7,14 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 interface IFxsClaim{
     function claimFees(address _distroContract, address _token) external;
 }
+interface IFxsProxy{
+    function operator() external view returns(address);
+}
 
 //hook that claims vefxs fees
 contract FXSRewardHook is IRewardHook{
 
-    address public constant booster = address(0xA2cF21b157b2f203e37b616b619f438B5aa86Ee5);
+    address public constant voteproxy = address(0x59CFCD384746ec3035299D90782Be065e466800B);
     address public constant fxs = address(0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0);
     address public constant distro = address(0xc6764e58b36e26b08Fd1d2AeD4538c02171fA872);
     address public constant stash = address(0x4f3AD55D7b884CDC48ADD1e2451A13af17887F26);
@@ -32,8 +35,10 @@ contract FXSRewardHook is IRewardHook{
     function onRewardClaim() override external{
         require(msg.sender == prevhook,"!auth");
 
-        IFxsClaim(booster).claimFees(distro,fxs);
+        //ask the current operator to claim fees
+        IFxsClaim( IFxsProxy(voteproxy).operator() ) .claimFees(distro,fxs);
 
+        //if reward hook is in place, call
         if(rewardHook != address(0)){
             try IRewardHook(rewardHook).onRewardClaim(){
             }catch{}
