5a1260f46a2652fb131de9b7d494180f30238879
C2tP <c2tp@protonmail.com>
2023-02-07 10:48:25 +0900

    stash token revert on transferfrom

--- contracts/contracts/StashTokenWrapper.sol
+++ contracts/contracts/StashTokenWrapper.sol
@@ -50,6 +50,8 @@ contract StashTokenWrapper {
     }
 
     function approve(address spender, uint256 amount) external returns (bool){ return true; }
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) { return true; }
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        revert("sealed");
+    }
 
 }
