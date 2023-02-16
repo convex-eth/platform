8e129aa8bee8ec315e43be14421c084244c93280
C2tP <c2tp@protonmail.com>
2023-02-16 22:35:43 +0900

    redirect fix

--- contracts/contracts/wrappers/ConvexStakingWrapper.sol
+++ contracts/contracts/wrappers/ConvexStakingWrapper.sol
@@ -427,8 +427,8 @@ contract ConvexStakingWrapper is ERC20, ReentrancyGuard {
     function getReward(address _account) external {
         //check if there is a redirect address
         address claimTo = _account;
-        if(rewardRedirect[msg.sender] != address(0)){
-            claimTo = rewardRedirect[msg.sender];
+        if(rewardRedirect[_account] != address(0)){
+            claimTo = rewardRedirect[_account];
         }
         //claim directly in checkpoint logic to save a bit of gas
         _checkpointAndClaim([_account, claimTo]);
