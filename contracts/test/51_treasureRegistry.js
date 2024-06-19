55fee02bc5471b6f32c3cec65384bc98c898b020
C2tP <c2tp@protonmail.com>
2024-06-19 10:29:48 +0900

    treasury remove add test script

---
+++ contracts/test/51_treasureRegistry.js
@@ -0,0 +1,184 @@
+// const { BN, constants, expectEvent, expectRevert, time } = require('openzeppelin-test-helpers');
+const { BN, time } = require('openzeppelin-test-helpers');
+var jsonfile = require('jsonfile');
+var contractList = jsonfile.readFileSync('./contracts.json');
+
+const Booster = artifacts.require("Booster");
+const CrvDepositor = artifacts.require("CrvDepositor");
+const ConvexToken = artifacts.require("ConvexToken");
+const cvxCrvToken = artifacts.require("cvxCrvToken");
+const CurveVoterProxy = artifacts.require("CurveVoterProxy");
+const BaseRewardPool = artifacts.require("BaseRewardPool");
+const CvxCrvStakingWrapper = artifacts.require("CvxCrvStakingWrapper");
+const IERC20 = artifacts.require("IERC20");
+const ICurveAavePool = artifacts.require("ICurveAavePool");
+const IExchange = artifacts.require("IExchange");
+const IUniswapV2Router01 = artifacts.require("IUniswapV2Router01");
+const CvxMining = artifacts.require("CvxMining");
+const ICurveRegistry = artifacts.require("ICurveRegistry");
+const DebugRegistry = artifacts.require("DebugRegistry");
+const TreasuryLend = artifacts.require("TreasuryLend");
+const TreasuryRegistry = artifacts.require("TreasuryRegistry");
+const TreasuryFunds = artifacts.require("TreasuryFunds");
+
+const unlockAccount = async (address) => {
+  let NETWORK = config.network;
+  if(!NETWORK.includes("debug")){
+    return null;
+  }
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "hardhat_impersonateAccount",
+        params: [address],
+        id: new Date().getTime(),
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
+};
+
+const setNoGas = async () => {
+  let NETWORK = config.network;
+  if(!NETWORK.includes("debug")){
+    return null;
+  }
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "hardhat_setNextBlockBaseFeePerGas",
+        params: ["0x0"],
+        id: new Date().getTime(),
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
+};
+
+const send = payload => {
+  if (!payload.jsonrpc) payload.jsonrpc = '2.0';
+  if (!payload.id) payload.id = new Date().getTime();
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(payload, (error, result) => {
+      if (error) return reject(error);
+
+      return resolve(result);
+    });
+  });
+};
+
+/**
+ *  Mines a single block in Ganache (evm_mine is non-standard)
+ */
+const mineBlock = () => send({ method: 'evm_mine' });
+
+/**
+ *  Gets the time of the last block.
+ */
+const currentTime = async () => {
+  const { timestamp } = await web3.eth.getBlock('latest');
+  return timestamp;
+};
+
+/**
+ *  Increases the time in the EVM.
+ *  @param seconds Number of seconds to increase the time by
+ */
+const fastForward = async seconds => {
+  // It's handy to be able to be able to pass big numbers in as we can just
+  // query them from the contract, then send them back. If not changed to
+  // a number, this causes much larger fast forwards than expected without error.
+  if (BN.isBN(seconds)) seconds = seconds.toNumber();
+
+  // And same with strings.
+  if (typeof seconds === 'string') seconds = parseFloat(seconds);
+
+  await send({
+    method: 'evm_increaseTime',
+    params: [seconds],
+  });
+
+  await mineBlock();
+};
+contract("Test supplying crvusd to llamalend", async accounts => {
+  it("should test supplying crvusd on behalf of treasury", async () => {
+
+    let deployer = "0x947B7742C403f20e5FaCcDAc5E092C943E7D0277";
+    await unlockAccount(deployer);
+    let multisig = "0xa3C5A1e09150B75ff251c1a7815A07182c3de2FB";
+    await unlockAccount(multisig);
+    let addressZero = "0x0000000000000000000000000000000000000000"
+
+
+    //system
+    let booster = await Booster.at(contractList.system.booster);
+    let voteproxy = await CurveVoterProxy.at(contractList.system.voteProxy);
+    let cvx = await ConvexToken.at(contractList.system.cvx);
+    let crv = await IERC20.at("0xD533a949740bb3306d119CC777fa900bA034cd52");
+    let cvxCrv = await IERC20.at("0x62B9c7356A2Dc64a1969e19C23e4f579F9810Aa7");
+    let userA = accounts[0];
+    let userB = accounts[1];
+    let userC = accounts[2];
+    let userD = accounts[3];
+    let userZ = "0xAAc0aa431c237C2C0B5f041c8e59B3f1a43aC78F";
+    var userNames = {};
+    userNames[userA] = "A";
+    userNames[userB] = "B";
+    userNames[userC] = "C";
+    userNames[userD] = "D";
+    userNames[userZ] = "Z";
+
+    const advanceTime = async (secondsElaspse) => {
+      // await time.increase(secondsElaspse);
+      // await time.advanceBlock();
+      await fastForward(secondsElaspse);
+      console.log("\n  >>>>  advance time " +(secondsElaspse/86400) +" days  >>>>\n");
+
+      // var periodend = await vanillacvxCrv.periodFinish();
+      // var now = Number( (new Date()).getTime() / 1000 ).toFixed(0);
+      // console.log("period end: " +Number(periodend))
+      // console.log("now: " +now)
+      // if(Number(periodend) < now){
+      //   await booster.earmarkRewards(100);
+      //   console.log("\n  >>>>  harvested");
+      // }
+    }
+    const day = 86400;
+
+    var registry = await TreasuryRegistry.at(contractList.system.treasuryRegistry);
+    var treasury = await TreasuryFunds.at(contractList.system.treasury);
+
+    await registry.owner().then(a=>console.log("owner: " +a));
+    await registry.registryList().then(a=>console.log(JSON.stringify(a)));
+
+    var removereg = registry.contract.methods.removeFromRegistry(3).encodeABI();
+    console.log("remove: " +removereg);
+    await treasury.execute(registry.address,0,removereg,{from:multisig,gasPrice:0})
+    console.log("called")
+
+    await registry.registryList().then(a=>console.log(JSON.stringify(a)));
+
+    var addreg = registry.contract.methods.addToRegistry("0x04Dd97255ddeE29c941D85F5B5cdE6ace8BD207f").encodeABI();
+    console.log("add: " +addreg)
+    await treasury.execute(registry.address,0,addreg,{from:multisig,gasPrice:0})
+    console.log("called")
+
+    await registry.registryList().then(a=>console.log(JSON.stringify(a)));
+
+  });
+});
+
+
